6d3388f89395056a5104515f58c20a64e7037d54
Jon Dimaculangan <jondmclngn@gmail.com>
2022-02-25 17:56:44 -0600

    erd added

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
+//IMPORTS
 const express = require('express')
 const app = express()
 const ejsLayouts = require('express-ejs-layouts')
+const cookieParser = require('cookie-parser')
+const cryptoJs = require('crypto-js')
+const db = require('./models/index.js')
 
 require('dotenv').config() //allows us to access env vars
 
-app.set('view engine', 'ejs')
-app.use(ejsLayouts)
+app.set('view engine', 'ejs') // set the view engine to ejs
+app.use(ejsLayouts) // tells express we want to user layouts
+app.use(cookieParser()) //cookie parser => give access to req.cookies
+app.use(express.urlencoded({ extended: false })) //body parser => give access to req.body
+
+//CUSTOM LOGIN MIDDLEWARE
+app.use(async (req, res, next) => {
+    if (req.cookies.userId) {
+        //decrypting the incoming user id from the cookie
+        const decryptedId = cryptoJs.AES.decrypt(req.cookies.userId, process.env.SECRET)
+        //converting the decrypted id into a readable string
+        const decryptedIdString = decryptedId.toString(cryptoJs.enc.Utf8)
+        // querying the db for the user with that id
+        const user = await db.user.findByPk(decryptedIdString)
+        // assigning the found user to res.locals.user in the routes, and user in the ejs
+        res.locals.user = user
+    } else res.locals.user = null
+    next()// move on to next middleware
+})
+
+//Controllers
+app.use('/users', require('./controllers/users.js'))
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
 })
+
 
 //check for an env PORT, otherwise use 8000
 const PORT = process.env.PORT || 8000
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # Project 2 Auth
+
+## ERDs
+
+![an ERD of my project](./erd.drawio.png)
